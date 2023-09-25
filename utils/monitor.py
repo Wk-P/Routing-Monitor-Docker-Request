b3b09e6cf7cf7cee5584ff6bf76a17a2c53fb6da
Wk-P <weekend2000119@gmail.com>
2023-09-25 18:25:21 +0900

    Routing

---
+++ utils/monitor.py
@@ -0,0 +1,115 @@
+import docker, time, json
+
+# get address
+def get_all_worker_node_address(nodes):
+    nodes_info_pool = []
+    for node in nodes:
+        if node.attrs['Spec']['Role'] == "worker":
+            # test
+            print(f"{node.attrs['Description']['Hostname']}: {node.attrs['Status']['Addr']}")
+
+            ip = node.attrs['Status']['Addr']
+            port = 2375
+
+            node_info = {
+                "name": node.attrs['Description']['Hostname'],
+                "address": f'{ip}:{port}'
+            }
+
+            nodes_info_pool.append(node_info)
+
+    return nodes_info_pool
+
+
+
+# get client
+def get_all_nodes_client(nodes_info):
+
+    for node in nodes_info:
+        node["client"] = docker.DockerClient(base_url=f"tcp://{node['address']}")
+
+    return nodes_info
+
+
+# find container by node_name and service name
+def get_container_of_service(nodes_info, service_name):
+    for node in nodes_info:
+        node['containers'] = list()
+        containers = node['client'].containers.list()
+
+        # get the container we want
+        
+        for c in containers:
+            container_name = c.name
+            if container_name.split('.')[0] == service_name:
+                try:
+                    # add container object in nodes_info
+                    node['containers'].append(c)
+                except:
+                    print(c)
+                    exit(1)
+    return None
+
+
+
+
+# def update_data(client, container, manager_client, service_name, scaled):
+def monitor_node(nodes_info):
+    for node in nodes_info:
+        client = node['client']
+
+        for container in node['containers']:
+            # get CPU stats data
+            stats_data = client.containers.get(container.id).stats(stream=False)
+
+            pre_cpu_stats = stats_data['precpu_stats']
+            cpu_stats = stats_data['cpu_stats']
+
+            cpu_usage = cpu_stats['cpu_usage']
+            pre_cpu_usage = pre_cpu_stats['cpu_usage']
+
+            system_cpu_usage = cpu_stats['system_cpu_usage']
+            online_cpus = cpu_stats['online_cpus']
+
+            # calculate
+            cpu_delta = cpu_usage['total_usage'] - pre_cpu_usage['total_usage']
+            system_delta = system_cpu_usage - pre_cpu_stats['system_cpu_usage']
+
+            cpu_percent = 0.0
+            if system_delta > 0.0:
+                cpu_percent = (cpu_delta / system_delta) * online_cpus * 100
+
+            print(f"Node {node['name']} Container {container.name.split('.')[0]} CPU Percent is {cpu_percent: .2f} %")
+            
+            # scale
+            if cpu_percent >= 10:
+                # route_table change
+                return {'node': node['name'], 'status': 'running', 'request': 'HS'}
+
+
+
+def create_monitor():
+    default_client = docker.from_env()
+    service_name = "node-service"
+    nodes = default_client.nodes.list()
+
+    # get node requests url and port (json list : NAME, ADDRESS)
+    nodes_info = get_all_worker_node_address(nodes)
+
+    # get client for every node (json list: NAME, ADDRESS, CLIENT)
+    nodes_info = get_all_nodes_client(nodes_info)
+
+    get_container_of_service(nodes_info=nodes_info, service_name=service_name)
+
+    return nodes_info
+
+def start_monitor(nodes_info):
+   
+    while True:
+        # print nodes CPU percent
+        # get router table change request
+        # ret == node name
+        yield monitor_node(nodes_info=nodes_info)
+
+if __name__ == "__main__":
+    start_monitor()
---
+++ utils/router.py
@@ -0,0 +1,157 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import requests
+import traceback
+from monitor import start_monitor, create_monitor
+import threading
+import logging
+
+route_table = [
+        {
+            'node': 'ubuntuDockerWorker', 
+            'ip': '192.168.56.103', 
+            'port': 8080,
+            'status': 'N'
+        },
+        {
+            'node': 'ubuntuDockerWorker1',
+            'ip': '192.168.56.104', 
+            'port': 8080,
+            'status': 'Y'
+        }
+    ]
+
+
+class RoutingHandler(BaseHTTPRequestHandler):
+    
+    def __init__(self, request, client_address, server):
+        self.route_table = route_table
+        super().__init__(request, client_address, server)
+
+    def log_message(self, format, *args):
+        # print(self.headers['Host'])
+        pass
+    
+
+    def __parse(self):
+        content_length = int(self.headers['Content-Length'])
+        get_data = self.rfile.read(content_length)
+
+        # parse to json
+        data = json.loads(get_data)
+        return data
+
+    def do_GET(self):
+        target_address = None
+
+        for node in route_table:
+            if node['status'] == 'Y':
+                target_address = f"{node['ip']}:{node['port']}" 
+                break
+
+
+        if target_address is not None:
+            url = f"http://{target_address}"
+
+            response_content = ""
+
+            error_response = {
+                'code': 500,
+                'status': 'failed'
+            }
+
+
+            try:
+                response_from_server = requests.get(url, json=self.__parse())
+
+                response_content = response_from_server.text
+
+            except:
+                response_content = json.dumps(error_response)
+                print('Response Error!')
+
+        else:
+            response_content = json.dumps(error_response)
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+
+        self.wfile.write(response_content.encode('utf-8'))
+
+
+def create_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    logger.addHandler(console_handler)
+    return logger
+
+
+def server_run(logger):
+
+    try:
+        router_address = ('192.168.56.102', 8080)
+        httpd = HTTPServer(router_address, RoutingHandler)
+
+        logger.debug("Router Started...")
+        httpd.serve_forever()
+    except:
+        traceback.print_exc()
+        logger.debug("Router stopped")
+
+
+def monitor_run(logger):
+    # with yield in <start_monitor>
+    generator = start_monitor(nodes_info=create_monitor())
+    while True:
+        # msg content style:
+        #   {'node': node['name'], 'status': 'running', 'request': 'HS'}
+        #
+        msg = next(generator)
+        if msg['request'] == 'HS':
+            change_node_status(route_table=route_table)
+
+        logger.debug(msg)
+        logger.debug(route_table)
+
+
+def change_node_status(route_table):
+
+    ret = {
+        'node': "",
+        'operation': 'status change',
+        'result': 'None'
+    }
+
+    for node_obj in route_table:
+        # start node to run
+        ret['node'] = node_obj['node']
+        if node_obj['status'] == 'N':
+            node_obj['status'] = 'Y'
+            ret['result'] = 'success'
+        else:
+            ret['result'] = 'failed'
+
+    return ret
+
+
+def main():
+    server_logger = create_logger("ServerLogger")
+    monitor_logger = create_logger("MonitorLogger")
+
+    server_thread = threading.Thread(target=server_run, args=((server_logger, )))
+    monitor_thread = threading.Thread(target=monitor_run, args=((monitor_logger, )))
+
+    monitor_thread.daemon = True
+
+    server_thread.start()
+    monitor_thread.start()
+
+if __name__ == "__main__":
+    
+    try:
+        main()
+    except:
+        print("Router stopped")
+    
