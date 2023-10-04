1cd0be84cde5747a3977c097fc7f8a0355033012
Wk-P <weekend2000@gmail.com>
2023-10-04 18:24:33 +0900

    update from PC

--- utils/router.py
+++ utils/router.py
@@ -1,9 +1,18 @@
 import aiohttp
 import asyncio
 from aiohttp import web
-from monitor import create_monitor, active_node, monitor_node
 import multiprocessing
-import os
+
+route_table = [
+    {
+        "address": "http://192.168.56.103:8080",
+        'status': "N"
+    },
+    {
+        "address": "http://192.168.56.104:8080",
+        'status': "Y"
+    }
+]
 
 def update_route_table(shared_route_table):
     # 在主进程中更新共享的route_table
@@ -18,6 +27,20 @@ async def web_app(shared_route_table):
         # 这里可以访问共享的route_table，根据需要进行处理
         await asyncio.sleep(1)
 
+async def handle_request(request):
+    # 在处理请求时使用共享的route_table
+    global server_index
+    server_url = None
+
+    while True:
+        if shared_route_table[server_index]['status'] == 'Y':
+            server_url = shared_route_table[server_index]['address']
+            break
+        server_index = (server_index + 1) % len(shared_route_table)
+
+    # 其余的请求处理逻辑不变
+    return web.Response(text=f"Forwarded to {server_url}")
+
 if __name__ == "__main__":
     manager = multiprocessing.Manager()
     shared_route_table = manager.list(route_table)  # 创建一个共享的列表
@@ -25,23 +48,6 @@ if __name__ == "__main__":
     # 启动一个进程来更新route_table
     update_process = multiprocessing.Process(target=update_route_table, args=(shared_route_table,))
     update_process.start()
-
-    nodes_info = create_monitor()
-
-    server_index = 0
-
-    async def handle_request(request):
-        # 在处理请求时使用共享的route_table
-        global server_index
-        server_url = None
-
-        while True:
-            if shared_route_table[server_index]['status'] == 'Y':
-                server_url = shared_route_table[server_index]['address']
-                break
-            server_index = (server_index + 1) % len(shared_route_table)
-
-        # 其余的请求处理逻辑不变
 
     # 创建一个事件循环
     loop = asyncio.get_event_loop()
@@ -51,5 +57,7 @@ if __name__ == "__main__":
 
     app = web.Application()
     app.router.add_post('/', handle_request)
+
+    server_index = 0  # 这个变量需要在全局中声明
 
     web.run_app(app, host='192.168.56.102', port=8080)
