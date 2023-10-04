d7ef7328c3a50da2c269c7fff65f24893b353624
Wk-P <weekend2000119@gmail.com>
2023-10-04 20:04:21 +0900

    update from linux

--- utils/router.py
+++ utils/router.py
@@ -2,22 +2,23 @@ import aiohttp
 import asyncio
 from aiohttp import web
 import multiprocessing
-from multiprocessing import Manager, Lock
+from multiprocessing import Manager
 from monitor import create_monitor, active_node, monitor_node
-
 
 
 async def handle_request(request):
     global server_index
     server_url = None
 
-    # print(f"In handle {route_table}") 
+    table = queue.get()
+    print(f"In handle {table}") 
     while True:
-        if route_table[server_index]['status'] == 'Y':
-            server_url = route_table[server_index]['address']
+        if table[server_index]['status'] == 'Y':
+            server_url = table[server_index]['address']
             break
         server_index = (server_index + 1) % len(route_table)
 
+    queue.put(table)
     async with aiohttp.ClientSession() as session:
         async with session.request(
             method="GET",
@@ -39,6 +40,7 @@ async def handle_request(request):
 
 # update routing table function
 def active_one_node(nodes_info):
+    table = queue.get()
     print(f"In active {route_table}") 
     node_address = None
     for node in route_table:
@@ -47,6 +49,7 @@ def active_one_node(nodes_info):
             node['status'] = 'Y'
             print(f"In active {route_table}") 
 
+    queue.put(table)
 
     if node_address is not None:
         for node in nodes_info:
@@ -71,12 +74,19 @@ def process_for_monitor(nodes_info, route_table):
                 pass
 
 
+
+def process_for_server(route_table):
+    app = web.Application()
+    app.router.add_post('/', handle_request)
+
+    web.run_app(app, host='192.168.56.102', port=8080)
+
 if __name__ == "__main__":
     # Create a multiprocessing Manager for shared data
     manager = Manager()
-    lock = Lock()
+    queue = multiprocessing.Queue()
     # Use a Manager list for route_table to make it shared between processes
-    route_table = manager.list([
+    route_table = manager,list([
         {
             "address": "http://192.168.56.103:8080",
             'status': "N"
@@ -86,16 +96,16 @@ if __name__ == "__main__":
             'status': "Y"
         }
     ])
+    
+    queue.put(route_table)
 
     nodes_info = create_monitor()
 
     server_index = 0
 
     # Create a separate process for process_for_monitor
-    monitor_process = multiprocessing.Process(target=process_for_monitor, args=(nodes_info, route_table))
+    monitor_process = multiprocessing.Process(target=process_for_monitor, args=(nodes_info, route_table, ))
     monitor_process.start()
 
-    app = web.Application()
-    app.router.add_post('/', handle_request)
-
-    web.run_app(app, host='192.168.56.102', port=8080)
+    server_process = multiprocessing.Process(target=process_for_server, args=(route_table, ))
+    server_process.start()
