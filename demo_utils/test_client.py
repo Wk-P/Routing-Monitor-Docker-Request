1b178ae2f3df165eb1d242b2f25ecdba4357a0c1
Wk-P <weekend2000119@gmail.com>
2024-05-16 10:00:12 +0900

    update files

--- demo_utils/test_client.py
+++ demo_utils/test_client.py
@@ -3,7 +3,6 @@ import requests
 import concurrent.futures
 import time
 import typing
-import pandas as pd
 
 
 def send(n):
@@ -13,8 +12,15 @@ def send(n):
         
         headers = {'task_type': "C"}
         data = {"number": n}
+        
 
-        return requests.post(url=f"http://{host}:{port}", headers=headers, data=data).json()
+        start = time.time()
+        response = requests.post(url=f"http://{host}:{port}", headers=headers, data=data).json()
+
+        return {
+            "response": response,
+            "run-time": time.time() - start,
+        }
 
     except Exception as e:
         print(e)
@@ -25,8 +31,10 @@ def process():
     recv_result = 0
     with concurrent.futures.ProcessPoolExecutor() as e:
         futures:typing.List[concurrent.futures.Future] = []
-        for _ in range(1):
-            futures.append(e.submit(send))
+        for _ in range(20):
+            futures.append(e.submit(send, 10000000))
+            time.sleep(1)
+
             
         for future in concurrent.futures.as_completed(futures):
             print(future.result())
@@ -38,25 +46,12 @@ def process():
 
 
 
-
 if __name__ == "__main__":
 
     # process()
     print("Running...")
-    numbers = [100000, 10000000]
-    cpu = 50
-    data = []
-    print(send(10))
-    # for num in numbers:
-    #     for _ in range(10):
-    #         start_time = time.time()
-    #         send(num)
-    #         runtime = round(time.time() - start_time, 5)
-    #         data.append({
-    #             "number": num,
-    #             "cpu": cpu, 
-    #             "time": runtime,
-    #         })
-    #     df = pd.DataFrame(data)
+    # numbers = [100000, 10000000]
+    # cpu = 50
+    # data = []
     
-    # df.to_excel("cpu50_output.xlsx", index=False)
+    process()
--- utils/hs_router1.py
+++ utils/hs_router1.py
@@ -1,14 +1,16 @@
 import aiohttp
 from aiohttp import web
 import multiprocessing
+import requests
+
 
 class Info():
     def __init__(self):
-        self.ips = [
-            "10.0.2.7",
-            "10.0.2.6",
-            "10.0.2.8"
-        ]
+        self.ips = {
+            "10.0.2.7": 0,
+            "10.0.2.8": 0,
+            "10.0.2.5": 0,
+        }
         self.port = 8080
         self.session = None
 
@@ -16,7 +18,44 @@ class Info():
         self.session = aiohttp.ClientSession()
 
 
+def query_prometheus(query: str):
+    prometheus_url = "http://localhost:9090/api/v1/query"
+
+    params = {'query': query}
+
+    response = requests.get(prometheus_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['result']
+    else:
+        print("Error: Unable to fetch data from Prometheus")
+        return None
+
+
+
+def url_choose():
+    global info_obj
+    ip = None
+    query_expression = r"100 - (avg by (instance) (irate(node_cpu_seconds_total{job='ubuntuDockerWorker',mode='idle'}[10m])) * 100)"
+    results = query_prometheus(query_expression)
+    min_usage = 100
+    min_usage_index = 0
+
+    for i in range(len(results)):
+        info_obj.ips[results[i]['metric']['instance'][:-5]] = float(results[i]['value'][1])
+        if float(results[i]['value'][1]) < min_usage:
+            min_usage = float(results[i]['value'][1])
+            min_usage_index = i
+    
+    ip = results[min_usage_index]['metric']['instance'][:-5]
+
+
+    return ip
+
+
 info_obj = Info()
+
 
 async def request_handler(request: web.Request):
     global info_obj
@@ -24,16 +63,20 @@ async def request_handler(request: web.Request):
     data = await request.post()
 
     # url choose
-    url = f"http://{info_obj.ips[2]}:{info_obj.port}"
+    ip = url_choose()
+    url = f"http://{ip}:{info_obj.port}"
 
-    print(url)
-    print(info_obj.session)
+    # query_expression = r"100 - (avg by (instance) (irate(node_cpu_seconds_total{job='ubuntuDockerWorker',mode='idle'}[10m])) * 100)"
+    # usages_list = query_prometheus(query_expression)
+    usages = info_obj.ips
+   
 
     async with info_obj.session.post(url=url, data=data, headers=headers) as response:
         
-        response_data = await response.json()
-
-        return web.json_response({"success": 1, "response": response_data})
+        data = await response.json()
+        res = {"success": 1, "response": data, "ip": ip}
+        res.update({'usages':usages})
+        return web.json_response(res)
 
 
 
@@ -49,7 +92,7 @@ async def server_app_init():
 
 def server_proc():
     app = server_app_init()
-    web.run_app(app, host='10.0.2.9', port=8081)
+    web.run_app(app, host='192.168.56.107', port=8081)
 
 
 if __name__ == "__main__":
