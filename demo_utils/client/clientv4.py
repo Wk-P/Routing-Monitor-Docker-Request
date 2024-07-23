888aba3eee63255969bcf175f08291a3c4491575
Wk-P <weekend2000@gmail.com>
2024-07-23 21:31:19 +0900

    update files

--- demo_utils/client/clientv4.py
+++ demo_utils/client/clientv4.py
@@ -3,48 +3,58 @@ import time
 import typing
 import random
 from openpyxl import Workbook # type: ignore
+from openpyxl import load_workbook
 import aiohttp
 import asyncio
 import os
+from datetime import datetime
+from pathlib import Path
 
 send_cnt = 0
 finished_cnt = 0
-requests_sum = 1200
-filename = "response_information_v1(1200)"
-dirpath = "excel4"
+loops = 20
+requests_batch = 150
+all_requests_sum = loops * requests_batch
+
+# filename = "response_information_v1(150)"
+filename = f"test({requests_batch}#{datetime.ctime(datetime.now()).replace(' ', '-').replace(':', '-')})"
+dirpath = Path.cwd() / "excel4"
 
 def to_excel(data, filename, dirpath, headers):
-
+    print(filename, dirpath)
     if not os.path.exists(dirpath):
         os.makedirs(dirpath, exist_ok=True)
 
-    workbook = Workbook()
-    sheet = workbook.active
+    file_path = str(dirpath / f"{filename}.xlsx")
 
-    # | user-response-time | request-number | response-ip   | process-time  |
-    # | 0.3                | 10000          | 192.168.0.150 | 14.523432     |
-    # | 0.5                | 20000          | 192.168.0.151 | 9.5232642     |
+    if os.path.exists(file_path):
+        # 如果文件存在，加载工作簿和活动工作表
+        workbook = load_workbook(file_path)
+        sheet = workbook.active
 
-    sheet.append(
-        [ header for header in headers ]
-    )
+    else:
+        # 如果文件不存在，创建新的工作簿和工作表
+        workbook = Workbook()
+        sheet = workbook.active
+        # 写入表头
+        sheet.append(headers)
 
     for row in data:
         sheet.append(row)
 
-    workbook.save(filename=f"{dirpath}/{filename}.xlsx")
+    workbook.save(file_path)
 
 
 async def fetch(session: aiohttp.ClientSession, url, number):
     global finished_cnt
-    global requests_sum
+    global requests_batch
     global send_cnt
     data = {"number": number}
     headers = {"task-type": "C"}
 
     send_cnt += 1
 
-    print(f"Send count: {send_cnt}/{requests_sum}")
+    print(f"Send count: {send_cnt}/{all_requests_sum}")
 
     start_time = time.time()
 
@@ -53,9 +63,8 @@ async def fetch(session: aiohttp.ClientSession, url, number):
         data["total_response_time"] = time.time()  - start_time
         data['trans_delay'] = data['total_response_time'] - data["wait_time_in_worker_node"] - data["process_in_worker_node"]
 
-
         finished_cnt += 1
-        print(f'process information: {finished_cnt}/{requests_sum}')
+        print(f"process information: {finished_cnt}/{requests_batch}")
         return data
 
 
@@ -63,6 +72,7 @@ async def main(args):
     host = "192.168.0.100"
     port = 8081
     url = f"http://{host}:{port}"
+
 
     tasks = list()
     async with aiohttp.ClientSession(
@@ -78,63 +88,67 @@ async def main(args):
 
 async def run():
     # global variable
-    global requests_sum
+    global requests_batch
     global filename
     global dirpath
-    
+    global finished_cnt
 
-    # funciton
-    def result_parse(responses: typing.List[typing.Dict[str, typing.Any]]) -> typing.Tuple[int, typing.Dict[str, typing.Any], typing.List]:
-        data_table = list()
-        response_keys = list()
+    for _ in range(loops):
+        # funciton
+        def result_parse(responses: typing.List[typing.Dict[str, typing.Any]]) -> typing.Tuple[int, typing.Dict[str, typing.Any], typing.List]:
+            data_table = list()
+            response_keys = list()
 
-        for res in responses:
-            if res:
-                response_keys = list(res.keys())
+            for res in responses:
+                print(res)
+                if type(res)is not dict:
+                    response_keys = list(res.keys())
 
-        try:
-            if responses:
-                for response in responses:
-                    if response.get("success"):
-                        data_table.append(
-                            [value for key, value in response.items()]
-                        )
-                    else:
-                        data_table.append(["-" for _ in range(len(response_keys))])
-                print("--EXIT--")
-                code = 0
-            else:
-                code = 1
+            try:
+                if responses:
+                    for response in responses:
+                        if response.get("success"):
+                            data_table.append(
+                                [value for key, value in response.items()]
+                            )
+                        else:
+                            data_table.append(["-" for _ in range(len(response_keys))])
+                    print("--EXIT--")
+                    code = 0
+                else:
+                    code = 1
+                    data_table = None
+            except Exception as e:
+                print(e)
+                code = -1
                 data_table = None
-        except Exception as e:
-            print(e)
-            code = -1
-            data_table = None
-        finally:
-            return code, data_table, response_keys
-    
+            finally:
+                return code, data_table, response_keys
 
+        # process
+        # args = [random.randint(0, 1000000) for _ in range(requests_sum)]
+        args = [500000 for _ in range(requests_batch)]
 
-    # process
-    # args = [random.randint(0, 1000000) for _ in range(requests_sum)]
-    args = [500000 for _ in range(requests_sum)]
+        print("---start fetch---")
 
-    print("---start fetch---")
+        responses = await main(args)
 
-    responses = await main(args)
+        print("---generate data file---")
 
-    print("---generate data file---")
+        # write into excel file
+        code, data_table, col_headers = result_parse(responses)
 
-    # write into excel file
-    code, data_table, col_headers = result_parse(responses)
+        if data_table:
+            to_excel(data_table, filename, dirpath, col_headers)
+        else:
+            print("None data_table")
 
-    if data_table:
-        to_excel(data_table, filename, dirpath, col_headers)
-    else:
-        print("None data_table")
+        finished_cnt = 0
 
-    print(f"Cover finished\nExit code: {code}")
+        print(f"Cover finished\nExit code: {code}")
 
 
 if __name__ == "__main__":
     asyncio.run(run())
+
+    pass
