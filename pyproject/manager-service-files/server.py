988878ef1cd0c2299ae332b178694dd9f38a975a
Wk-P <weekend2000119@gmail.com>
2024-08-08 12:36:42 +0900

    update files

--- pyproject/manager-service-files/server.py
+++ pyproject/manager-service-files/server.py
@@ -2,7 +2,7 @@ import aiohttp
 from aiohttp import web
 import time
 from queue import Queue
-from xgboost import XGBRegressor
+from xgboost import XGBRegressor #type: ignore
 from pathlib import Path
 import threading
 from typing import List, Dict
