72a9e8f97baed03d83c0ea7d57abf31176a45058
Wk-P <weekend2000@gmail.com>
2025-02-17 13:41:13 +0900

    update files

--- clients_v2/client_src/client_zlx_v7.py
+++ clients_v2/client_src/client_zlx_v7.py
@@ -11,6 +11,7 @@ import numpy as np
 import logging
 import datetime
 import json
+import traceback
 
 # log file config
 PARENT_DIR = Path(__file__).parent.parent
@@ -22,7 +23,7 @@ logging.basicConfig(filename=log_path, level=logging.INFO, filemode='w')
 
 
 
-pic_path = (PARENT_DIR / 'figs' / f'{datetime.datetime.ctime(datetime.datetime.now()).replace(':', '').replace(' ', '_')}')
+pic_path = (PARENT_DIR / 'figs_1' / f'{datetime.datetime.ctime(datetime.datetime.now()).replace(':', '').replace(' ', '_')}')
 if not Path.exists(pic_path):
     Path(pic_path).mkdir(parents=True, exist_ok=True)
     print("Built Json and pictures path.")
@@ -53,36 +54,39 @@ class CustomClient:
         global FINISH_CNT, LOOPS, LOOP_FINISH_CNT, ERROR
 
         # recoard task start time
-        start = time.time()
+        start = time.perf_counter()
         task.data['request_id'] = index
         async with self.session.post(url=task.url, json=task.data, headers=task.headers) as response:
-            response_data = await response.json()
-            FINISH_CNT += 1
-            LOOP_FINISH_CNT += 1
-            print(f"\r{FINISH_CNT}/{tasks_sum * len(ALGO_NAMES) * LOOPS} {LOOP_FINISH_CNT}/{len(self.tasks)}")
-            print(f"\r{100 * FINISH_CNT/(tasks_sum * len(ALGO_NAMES) * LOOPS):.2f}% {100 * LOOP_FINISH_CNT/len(self.tasks):.2f}%")
-            response_data['response_time'] = time.time() - start        # recoard task end time
+            try:
+                response_data = await response.json()
+                FINISH_CNT += 1
+                LOOP_FINISH_CNT += 1
+                print(f"\r{FINISH_CNT}/{tasks_sum * len(ALGO_NAMES) * LOOPS} {LOOP_FINISH_CNT}/{len(self.tasks)}")
+                print(f"\r{100 * FINISH_CNT/(tasks_sum * len(ALGO_NAMES) * LOOPS):.2f}% {100 * LOOP_FINISH_CNT/len(self.tasks):.2f}%")
+                response_data['client_response_time'] = time.perf_counter() - start        # recoard task end time
 
-            logging.info(response_data)
-            
-            if algo_name == 'shortest':
-                logging.info(f"{response_data['status']} DIFF: {response_data['calculate_wait_time'] - response_data['real_wait_time']}")
-                calculate_wait_time = response_data['calculate_wait_time']
-                real_wait_time = response_data['real_wait_time']
-                error_time = abs(calculate_wait_time - real_wait_time)
-                ERROR['calculate_wait_time'].append(calculate_wait_time)
-                ERROR['real_wait_time'].append(real_wait_time)
+                logging.info(response_data)
+                
+                logging.info(f"{response_data}")
+                predict_waiting_time = response_data['predicted_waiting_time']
+                worker_queue_waiting_time = response_data['queue_waiting_time']
+                error_time = abs(predict_waiting_time - worker_queue_waiting_time)
+                ERROR['predicted_waiting_time'].append(predict_waiting_time)
+                ERROR['worker_queue_waiting_time'].append(worker_queue_waiting_time)
                 ERROR['error_time'].append(error_time)
 
-            # print(response_data)
-            return response_data
+                return response_data
+            except Exception as e :
+                error_msg = traceback.format_exc()
+                print(error_msg)
 
-    async def run_tasks(self, tasks_sum: int, algo_name: str):
+    async def run_tasks(self, tasks_sum: int, algo_name: str, random_task_interval_list: float):
         index = 0
         for task in self.tasks:
             self.tasks_corotine_list.append(asyncio.create_task(self.run_task(task, tasks_sum, algo_name, index)))
             print(f"\rSend Task {index + 1}")
-            await asyncio.sleep(self.task_interval)
+            # await asyncio.sleep(self.task_interval)
+            await asyncio.sleep(random_task_interval_list[index])
             index += 1
 
         # request order
@@ -132,6 +136,7 @@ def gen_tasks_poisson_1(n, *args, **kwargs):
     # 使用泊松分布生成 num_args 列表
     # generate num_args list with poisson
     num_args = np.random.poisson(lam=TASK_NUMBER_LAMBDA, size=n).tolist()
+
     # 生成任务列表
     # generate tasks list
     tasks = [Task(url=url, headers=headers, data={"number": arg}) for arg in num_args]
@@ -146,7 +151,7 @@ def gen_tasks_poisson_2(n, *args, **kwargs):
 
     # 使用泊松分布波动生成任务参数
     # generate task request number with poisson
-    num_args = [ int(np.random.poisson(lam=15000)) if num % 3 != 0 else int(np.random.poisson(lam=400000)) for num in range(n) ]
+    num_args = [ int(np.random.poisson(lam=200000)) if num % 3 != 0 else int(np.random.poisson(lam=500000)) for num in range(n) ]
     tasks = [Task(url=url, headers=headers, data={"number": arg}) for arg in num_args]
 
     return tasks
@@ -162,6 +167,8 @@ async def main(tasks_sum: List[int]):
 
         for tasks_sum in TASKS_SUM:
             
+            interval_list = [random.randint(500, 1000) / 1000 for _ in range(tasks_sum)]
+
             if POISSON_POLICY == 'poisson_1':
                 # poisson 1
                 tasks = gen_tasks_poisson_1(n=tasks_sum)
@@ -174,8 +181,9 @@ async def main(tasks_sum: List[int]):
                 tasks = gen_tasks(is_random=False, num_args=[15000 if num % 3 != 0 else 400000 for num in range(tasks_sum)], n=tasks_sum)
 
             client.tasks = tasks
-            
+
             for algo_name in ALGO_NAMES:
+
                 print(f"ALGO_NAME: {algo_name}")
 
                 start_time = time.time()
@@ -183,16 +191,17 @@ async def main(tasks_sum: List[int]):
                 
                 start_time = time.time()
 
-                await client.run_tasks(sum(TASKS_SUM), algo_name)
-                await asyncio.sleep(client.loop_interval)    
+                await client.run_tasks(sum(TASKS_SUM), algo_name, interval_list)
+                # await asyncio.sleep(client.loop_interval)
+
+                await asyncio.sleep(client.loop_interval)
                 LOOP_FINISH_CNT = 0
 
                 time_results[algo_name].append(time.time() - start_time)
 
                 # Error graph tables
-                if algo_name == 'shortest':
-                    error_graph(ERROR, f"{loop}_{algo_name}_{tasks_sum}", pic_path)
-                    ERROR = {key: [] for key in ["error_time", "calculate_wait_time", "real_wait_time"]}
+                error_graph(ERROR, f"{loop}_{algo_name}_{tasks_sum}", pic_path)
+                ERROR = {key: [] for key in ["error_time", "predicted_waiting_time", "worker_queue_waiting_time"]}
 
         print(time_results)
 
@@ -237,12 +246,13 @@ async def main(tasks_sum: List[int]):
 
 TASK_NUMBER_RANGE = (100000, 500000)
 # TASKS_SUM = [sum for sum in range(10, 400, 100)]
-TASKS_SUM = [10, 20]
+# TASKS_SUM = [10, 20, 30, 40]
 # TASKS_SUM = [30, 50, 100]
 # TASKS_SUM = [n for n in range(50, 401, 50)]
 # TASKS_SUM = [10, 30, 50, 100, 300, 500]
+TASKS_SUM = [500]
 TASK_INTERVAL = 0.05
-LOOPS = 3
+LOOPS = 20
 LOOP_INTERVAL = 2
 MANAGER_AGENT_IP = "192.168.0.100"
 MANAGER_AGENT_PORT = 8199
@@ -252,39 +262,35 @@ FINISH_CNT = 0
 LOOP_FINISH_CNT = 0
 # ALGO_NAMES = ['shortest', 'round-robin', 'least', 'lowest-score']
 # ALGO_NAMES = ['shortest', 'round-robin', 'min-entropy', 'least-connect']
-ALGO_NAMES = ['round-robin', 'min-entropy', 'least-connect']
-POISSON_POLICY = "possion_2"
+# ALGO_NAMES = ['round-robin', 'min-entropy', 'least-connect']
+ALGO_NAMES = ['shortest']
+POISSON_POLICY = ""
 
-ERROR = {key: [] for key in ["error_time", "calculate_wait_time", "real_wait_time"]}
+ERROR = {key: [] for key in ["error_time", "predicted_waiting_time", "worker_queue_waiting_time"]}
 
 
 # config
 config = {
     "POISSON_POLICY": POISSON_POLICY,
-    "TASK_NUMBER_RANGE": [100000, 500000],
-    "TASKS_SUM": [10, 50, 100, 500],
-    "TASK_INTERVAL": 0.01,
-    "LOOPS": 1,
-    "LOOP_INTERVAL": 2,
-    "MANAGER_AGENT_IP": "192.168.0.100",
-    "MANAGER_AGENT_PORT": 8199,
-    "URL": "http://192.168.0.100:8199",
-    "HEADERS": {
-        "task-type": "C"
-    },
-    "FINISH_CNT": 0,
-    "LOOP_FINISH_CNT": 0,
-    "ALGO_NAMES": [
-        "round-robin",
-        "min-entropy",
-        "least-connect"
-    ]
+    "TASK_NUMBER_RANGE": TASK_NUMBER_RANGE,
+    "TASKS_SUM": TASKS_SUM,
+    "TASK_INTERVAL": TASK_INTERVAL,
+    "LOOPS": LOOPS,
+    "LOOP_INTERVAL": LOOP_INTERVAL,
+    "MANAGER_AGENT_IP": MANAGER_AGENT_IP,
+    "MANAGER_AGENT_PORT": MANAGER_AGENT_PORT,
+    "URL": URL,
+    "HEADERS": HEADERS,
+    "FINISH_CNT": FINISH_CNT,
+    "LOOP_FINISH_CNT": LOOP_FINISH_CNT,
+    "ALGO_NAMES":ALGO_NAMES
 }
 
 
 
 def error_graph(error_dict: dict, suffix, _pic_path):
-    y_lists = list(error_dict.values())
+    # y_lists = list(error_dict.values())
+    y_lists = [error_dict["error_time"]]
 
     # ERROR BarChart
     data= {
@@ -295,7 +301,7 @@ def error_graph(error_dict: dict, suffix, _pic_path):
             y_lists,
         ],
         "titles": [
-            "Difference of time for real and pred",
+            "Difference of time for real and predicted",
         ],
         "xlabels": [
             "Task Index",
---
+++ clients_v2/client_src/client_zlx_v8.py
@@ -0,0 +1,208 @@
+from aiohttp import ClientSession, ClientTimeout, TCPConnector
+import asyncio
+import random
+import json
+from pathlib import Path
+import sys
+from datetime import datetime
+import matplotlib.pyplot as plt
+import traceback
+from time import perf_counter, sleep, time
+
+
+async def process_updater(total_tasks, shared_progress, interval=0.5):
+    while True:
+        done = shared_progress['completed']
+        percent = 100.0 * done / total_tasks
+        sys.stdout.write(f"\r => Completed {percent:.2f} %                ")
+        sys.stdout.flush()
+        if done >= total_tasks:
+            break
+        await asyncio.sleep(interval)
+    
+    print()
+
+async def send_request(**config):
+    request_number = random.randint(a=config.get("request_num_range")[0], b=config.get('request_num_range')[1])
+    request_id = config.get("request_id")
+    url = config.get("url")
+    session: ClientSession = config.get('session')
+    # request_count = config.get('request_count')
+    # request_sum = config.get('request_sum')
+    shared_progress: dict = config.get('shared_progress')
+    request_start_time = perf_counter()
+
+    async with session.post(url=url, json={"request_id": request_id,"number": request_number, "request_start_time": request_start_time}) as response:
+        response_data: dict = await response.json()
+        # request_count += 1
+        shared_progress['completed'] += 1
+
+    response_data['request_number'] = request_number
+    response_data['request_id'] = request_id
+
+    
+    response_time = perf_counter() - request_start_time
+    response_data['response_time'] = response_time
+
+    print(response_data)
+    return response_data
+
+def custom_serializer(obj):
+        if isinstance(obj, Path):
+            return str(obj)
+        raise TypeError(f"Type {type(obj)} not serializable.")
+
+def write_config_json_file(filename: Path, data):
+    filename = filename / "config"
+    if not filename.exists():
+        filename.mkdir(parents=True, exist_ok=True)
+
+    filename = filename / program_config.get("config_filename")
+
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=4, default=custom_serializer)
+
+
+def write_response_json_file(filename: Path, data):
+    filename = filename / "responses"
+    if not filename.exists():
+        filename.mkdir(parents=True, exist_ok=True)
+
+    filename = filename / program_config.get("response_filename")
+
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
+
+
+
+def draw(filename: Path, data: dict, title: str):
+    filename = filename / "figs"
+    if not filename.exists():
+        filename.mkdir(parents=True, exist_ok=True)
+
+    filename = filename / program_config.get("figs_filename")
+
+    plt.figure(figsize=(16, 9))
+    
+    for y_key, y in data.items():
+        plt.plot(range(len(y)), y, marker="o", label=y_key)
+
+    plt.title(title)
+    plt.xlabel("Index")
+    plt.ylabel("Seconds")
+    plt.xticks(rotation=45)  # 日期标签旋转
+    plt.grid(True)
+
+
+    # 添加图例
+    plt.legend()
+
+    # 调整布局并显示图表
+    plt.tight_layout()
+    plt.savefig(str(filename))
+
+
+async def main(**program_config):
+    request_sum = program_config.get('request_sum', 0)
+    request_num_range: tuple = program_config.get('request_num_range', 0)
+    request_count = program_config.get('request_count', 0)
+    url = program_config.get("url")
+    filename = program_config.get("filename")
+    shared_progress = program_config.get('shared_progress')
+
+    updater_task = asyncio.create_task(process_updater(request_sum, shared_progress, interval=0.5))
+
+    tasks = []
+
+    write_config_json_file(filename, program_config)
+
+    session = ClientSession(timeout=ClientTimeout(None), connector=TCPConnector(limit=0))
+
+    for request_index in range(request_sum):
+        request_id = request_index + 1
+        tasks.append(asyncio.create_task(send_request(session=session, url=url, shared_progress=shared_progress, request_num_range=request_num_range, request_id=request_id, request_sum=request_sum, request_count=request_count)))
+        
+        print(f"Send task {request_index}")
+        await asyncio.sleep(0.1)
+
+    # results: list[dict[str, float]] = await asyncio.gather(*tasks)
+
+
+    results = []
+    for task in asyncio.as_completed(tasks):
+        try:
+            result = await task
+            results.append(result)
+        except:
+            print(traceback.format_exc())
+            continue
+
+    await updater_task
+
+    # Prepare the response data
+    responses = {
+        # "request_id": [result.get('request_id') for result in results],
+        "request_number": [result.get('request_number') for result in results],
+        
+        "predicted_processing_time": [result.get("predicted_processing_time") for result in results],
+        "real_processing_time": [result.get("real_process_time") for result in results],
+        
+        # "predicted_waiting_time": [result.get("predicted_waiting_time") for result in results],
+
+        "response_time": [result.get("response_time") for result in results],
+
+        "waiting_jobs": [result.get("waiting_jobs") for result in results],
+        "real_waiting_time": [result.get("waiting_time") for result in results],
+
+        "user_cpu_time": [result.get("user_cpu_time") for result in results],
+        "system_cpu_time": [result.get("system_cpu_time") for result in results],
+        "cpu_spent_usage": [result.get('cpu_spent_usage') for result in results]
+    }
+
+    # Write response JSON file
+    write_response_json_file(filename, responses)
+
+    # Prepare the trend data for plotting
+    trend = {
+        # "request_id": [result.get('request_id') for result in results],
+        "request_number": [float(result.get('request_number') / 100000) for result in results],
+        
+        # "predicted_processing_time": [result.get("predicted_processing_time") for result in results],
+        "real_processing_time": [result.get("real_process_time") for result in results],
+        
+        # "predicted_waiting_time": [result.get("predicted_waiting_time") for result in results],
+
+        # "response_time": [result.get("response_time") for result in results],
+
+        # "waiting_jobs": [result.get("waiting_jobs") for result in results],
+        # "real_waiting_time": [result.get("waiting_time") for result in results],
+
+        # "user_cpu_time": [result.get("user_cpu_time") for result in results],
+        # "system_cpu_time": [result.get("system_cpu_time") for result in results],
+        # "cpu_spent_usage": [result.get('cpu_spent_usage') for result in results]
+    }
+    
+    title = "Predicted waiting time and real waiting time difference"
+    # Draw the trend comparison chart
+    draw(filename, trend, title)
+
+    await session.close()
+
+if __name__ == "__main__":
+    PARENT_DIR = Path(__file__).parent.parent
+    for loop in range(1, 2, 1):
+        program_config = {
+            "request_sum": loop * 50,
+            "request_count": 0,
+            "request_num_range": (0, 500000),
+            "url": "http://192.168.0.100:8199",
+            "filename": PARENT_DIR / "client_v3_data" / datetime.now().astimezone().strftime("%Y-%m-%d_%H-%M-%S"),
+            "response_filename": "response.json",
+            "figs_filename": "error.png",
+            "config_filename": "config.json",
+            "shared_progress": {"completed": 0}
+        }
+
+        asyncio.run(main(**program_config))
+
+        sleep(1)
