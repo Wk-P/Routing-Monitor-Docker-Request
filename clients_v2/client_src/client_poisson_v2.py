dfe3bdfb402a8064892d180b66e13983900bfe11
Wk-P <weekend2000119@gmail.com>
2025-03-17 14:02:02 +0900

    update files

--- clients_v2/client_src/client_poisson_v2.py
+++ clients_v2/client_src/client_poisson_v2.py
@@ -47,6 +47,7 @@ async def send_request(**config):
         # request_count += 1
         shared_progress['completed'] += 1
         
+        print(response_data)
 
         """ send from same time and longest response time """
         async_response_time = perf_counter() - request_start_time_table[request_id]
@@ -81,12 +82,12 @@ async def send_request(**config):
         """ version 0.0.2 (sum of manager response time) """
         if algo_name not in async_longest_response_time:
             sum_of_manager_response_time[algo_name] = {}
-            sum_of_manager_response_time[algo_name][worker_id] = response_data.get('no_waiting_response_time')
+            sum_of_manager_response_time[algo_name][worker_id] = response_data.get('predicted_processing_time')
         else:
             if worker_id not in sum_of_manager_response_time[algo_name]:
-                sum_of_manager_response_time[algo_name][worker_id] = response_data.get('no_waiting_response_time')
+                sum_of_manager_response_time[algo_name][worker_id] = response_data.get('predicted_processing_time')
             else:
-                sum_of_manager_response_time[algo_name][worker_id] += response_data.get('no_waiting_response_time')
+                sum_of_manager_response_time[algo_name][worker_id] += response_data.get('predicted_processing_time')
 
         return response_data
 
@@ -106,15 +107,15 @@ def write_config_json_file(filename: Path, data):
         json.dump(data, json_file, indent=4, default=custom_serializer)
 
 
-def write_response_json_file(filename: Path, data):
-    filename = filename / "responses"
-    if not filename.exists():
-        filename.mkdir(parents=True, exist_ok=True)
+# def write_response_json_file(filename: Path, data):
+#     filename = filename / "responses"
+#     if not filename.exists():
+#         filename.mkdir(parents=True, exist_ok=True)
 
-    filename = filename / program_config.get("response_filename")
+#     filename = filename / program_config.get("response_filename")
 
-    with open(filename, 'w') as json_file:
-        json.dump(data, json_file)
+#     with open(filename, 'w') as json_file:
+#         json.dump(data, json_file)
 
 
 
@@ -172,6 +173,54 @@ def draw_bar(filename: Path, data: dict, title: str):
     plt.close()
 
 
+# multi y-value
+def draw_bar2(filename: Path, data: list[dict], title: str):
+    print(data)
+    filename = filename / 'figs'
+    if not filename.exists():
+        filename.mkdir(parents=True, exist_ok=True)
+
+    filename = filename / program_config.get("figs_filename")  # 修正文件名为字符串
+
+    plt.figure(figsize=(16, 9))
+    
+    # 提取 X 轴名称（算法名称）
+    x_labels = sorted(set(key for item in data for key in item.keys()))
+
+
+    # 计算 X 轴索引
+    x_indexes = np.arange(len(x_labels))
+    width = 0.8 / max(1, len(data) + 1) # 柱状图的宽度
+
+    # 绘制柱状图
+    for i, item in enumerate(data):
+        y_values = [item.get(x, 0) for x in x_labels]  # 提取所有 y_key 对应的值
+        plt.bar(x_indexes + (i - len(data) / 2) * width, y_values, width=width, label=f"Dataset {i}")
+
+    # difference bar
+    if len(data) == 2:
+        diff_values = [abs(data[1].get(x, 0) - data[0].get(x, 0)) for x in x_labels]
+        plt.bar(x_indexes + (len(data) / 2) * width, diff_values, width=width, label="Difference", color='red')
+
+    # 设置 X 轴刻度
+    plt.xticks(ticks=x_indexes + width / 4, labels=x_labels)
+
+    plt.title(title)
+    plt.xlabel("Worker Node ID")
+    plt.ylabel("Total Response Time")
+    plt.grid(True)
+
+    # 添加图例
+    plt.legend()
+
+    # 调整布局并显示图表
+    plt.tight_layout()
+    plt.savefig(str(filename))
+    plt.close()
+
+
+
+
 async def main(**program_config):
     request_sum = program_config.get('request_sum', 0)
     request_num_range: tuple = program_config.get('request_num_range', 0)
@@ -179,7 +228,7 @@ async def main(**program_config):
     send_interval = program_config.get('send_interval', 0.01)
     request_count = program_config.get('request_count', 0)
     url = program_config.get("url")
-    filename = program_config.get("filename")
+    filename: dict = program_config.get("filename")
     shared_progress = program_config.get('shared_progress')
     algo_name = program_config.get('algo_name', None)
 
@@ -187,7 +236,7 @@ async def main(**program_config):
 
     tasks = []
 
-    write_config_json_file(filename, program_config)
+    write_config_json_file(filename.get("total_response_time", "default"), program_config)
 
     session = ClientSession(timeout=ClientTimeout(None), connector=TCPConnector(limit=0))
 
@@ -284,13 +333,16 @@ async def main(**program_config):
 
 if __name__ == "__main__":
     PARENT_DIR = Path(__file__).parent.parent
-    loops = 3
-    single_loop_task = 10
+    loops = 2
+    single_loop_task = 5
 
     algo_total_response_time_table = {} 
 
+
     for loop in range(1, loops + 1):
         time_temp = datetime.now().astimezone().strftime("%Y-%m-%d_%H-%M-%S")
+        folder_name = "221-diff-total"
+        default_path =  PARENT_DIR / "poisson_request_number" / folder_name / time_temp
         program_config = {
                 "request_sum": loop * single_loop_task,
                 "request_num_range": (0, 500000),
@@ -299,7 +351,12 @@ if __name__ == "__main__":
                 "figs_filename": "error.png",
                 "config_filename": "config.json",
                 "shared_progress": {"completed": 0},
-                "filename": PARENT_DIR / "poisson_request_number" / "total_response_time_v1_5_5" / time_temp,
+                "filename": 
+                    {
+                        "default": default_path,
+                        "total_response_time": default_path / "total",
+                        "difference_total_predicted_processing_time_and_total_real_processing_time":  default_path / "diff",
+                    },
                 "send_interval": send_interval,
             }
 
@@ -315,13 +372,13 @@ if __name__ == "__main__":
 
             results = asyncio.run(main(**program_config))
             
-            
+            # total response time of all requests for single algorithm
             algo_total_response_time = perf_counter() - algo_total_response_time_start
             algo_total_response_time_table[algo_name] = algo_total_response_time
 
             sleep(1)
 
-            print(f"\n[{algo_name}] All total response time: {algo_total_response_time}\n Single response time acc: {async_longest_response_time[algo_name]}s \n")
+            print(f"\n[{algo_name}] All total response time: {algo_total_response_time}\n Single response time acc: {async_longest_response_time[algo_name]}s\n Sum_of_manager_response_time: {sum_of_manager_response_time[algo_name]}\n")
 
             # save result into config.json
             result_info = {
@@ -333,7 +390,16 @@ if __name__ == "__main__":
             if "results" not in program_config:
                 program_config["results"] = {}
             program_config["results"][algo_name] = result_info
-            write_config_json_file(program_config["filename"], program_config)
+            write_config_json_file(program_config["filename"]['total_response_time'], program_config)
+
+            # difference of predicted data and real data
+            draw_bar2(filename=program_config['filename']['difference_total_predicted_processing_time_and_total_real_processing_time'] / algo_name, 
+                  data=[
+                      async_longest_response_time[algo_name],
+                      sum_of_manager_response_time[algo_name],
+                  ], 
+                  title=f"{len(algo_names)} total response time compare between real cumulative process time and predicted cumulative process time"
+                )
 
             print(f"Task count per worker: {len(results)} \n\n\n")
 
@@ -345,8 +411,9 @@ if __name__ == "__main__":
             async_longest_response_time[algo].clear()
             sum_of_manager_response_time[algo].clear()
 
-
-        draw_bar(filename=program_config['filename'], data=algo_total_response_time_table, title=f"{len(algo_names)} total response difference")
+        # total_response_graph
+        draw_bar(filename=program_config['filename']['total_response_time'], data=algo_total_response_time_table, title=f"{len(algo_names)} total response difference")
+        
         # 清理历史
         algo_total_response_time_table.clear()
         
